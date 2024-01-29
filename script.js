475f47deb8ab785ad180fb359828be5c57a773d1
Peter Lind <petl@kea.dk>
2024-01-29 14:07:19 +0100

    structure w functions

--- script.js
+++ script.js
@@ -5,3 +5,27 @@ window.addEventListener("load", start);
 function start() {
     console.log("JavaScript is running!");
 }
+
+function generateRandomNumber() {
+
+}
+
+function receiveGuess() {
+
+}
+
+function checkGuess() {
+
+}
+
+function guessIsCorrect() {
+
+}
+
+function guessIsTooLow() {
+
+}
+
+function guessIsTooHigh() {
+    
+}
